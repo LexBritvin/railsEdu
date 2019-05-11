c1b2f53a297e5e9725fb18fb41048378dd0629af
Aleksandr Britvin <lexbritvin@gmail.com>
2019-05-11 22:47:41 +0300

    Custom scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-
+  get 'angular-items', to: 'portfolios#angular'
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs do
