619d0c782ff3173df5838c45fc795528b440784f
Aleksandr Britvin <lexbritvin@gmail.com>
2019-05-12 15:51:13 +0300

    Working wih session

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
   get 'angular-items', to: 'portfolios#angular'
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs do
     member do
       get :toggle_status
